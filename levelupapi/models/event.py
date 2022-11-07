c5f172c42de271b465f19888a7a986a26465b9c0
Adam Featherston <j.adam.featherston@gmail.com>
2022-11-07 14:12:48 -0600

    serialized event with title of game and gamer who is hosting event

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 
+
 class Event(models.Model):
 
-    game = models.ForeignKey('Game', on_delete=models.CASCADE)
-    organizer = models.ForeignKey('Gamer', on_delete=models.CASCADE)
+    game = models.ForeignKey('Game', on_delete=models.CASCADE, related_name='game')
+    organizer = models.ForeignKey('Gamer', on_delete=models.CASCADE, related_name='gamer')
     description = models.CharField(max_length=155)
     date = models.DateField()
     time = models.TimeField()
